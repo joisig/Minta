814e812689dd8d2e8b6b7aa2d8fa88a48e2684ad
Snorri Örn Daníelsson <snorri@orninn.com>
2013-06-08 06:33:05 +0000

    small refactoring

--- popup.js
+++ popup.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
 	var currencies = undefined;
+	//Fetches newest exchange rate
 	function getLatestExt(){
 		$.ajax({
 			type: "GET",
 			url: 'http://apis.is/currency/m5'
 		}).done(function(data){
-			currencies = data.results;
+			enumerateSelect(data.results);
+		})
+	};
+	//Do this once
+	getLatestExt();
+	//Builds the selection box
+	function enumerateSelect(data){
+		currencies = data;
 			console.log(JSON.stringify(data.results));
 			for(var i = 0; i < currencies.length; ++i){
 					var currElement = $('#currencies');
 				currElement.append($("<option>").attr('value',currencies[i].value).text(currencies[i].shortName));
 			}
-		})
-	};
-	//Do this once
-	getLatestExt();
-
+	}
 });
