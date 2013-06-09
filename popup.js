c645febc453b753215186d676459db528e6a5c89
Snorri Örn Daníelsson <snorri@orninn.com>
2013-06-09 00:04:50 +0000

    Currency conversion is implemented.

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	var currencies = undefined;
+	var selectedCurr = undefined;
 	//Fetches newest exchange rate
 	function getLatestExt(){
 		$.ajax({
@@ -14,10 +15,31 @@ $(document).ready(function(){
 	//Builds the selection box
 	function enumerateSelect(data){
 		currencies = data;
-			console.log(JSON.stringify(data.results));
-			for(var i = 0; i < currencies.length; ++i){
-					var currElement = $('#currencies');
-				currElement.append($("<option>").attr('value',currencies[i].value).text(currencies[i].shortName));
-			}
+		console.log(JSON.stringify(data));
+		for(var i = 0; i < currencies.length; ++i){
+			var currElement = $('#currencies');
+			currElement.append($("<option>").attr('value',currencies[i].shortName).text(currencies[i].shortName));
+		};
+	 	selectedCurr = data[0];
 	}
+	//What happens when we change the currency type.
+	$('#currencies').change(function(){
+		var value = $(this).find(":selected").text();
+		for(var i = 0; i < currencies.length; ++i){
+			if(value === currencies[i].shortName){
+				selectedCurr = currencies[i];
+				break;
+			}
+		}
+	})
+	//What happens when we change the foreign amount
+	$('#foreign').on('keyup',function(){
+		var value = $(this).val();
+		$('#local').val(value*selectedCurr.value);
+	})
+	//What happens when we change the local amount
+	$('#local').on('keyup',function(){
+		var value = $(this).val();
+		$('#foreign').val(value/selectedCurr.value);
+	})
 });
