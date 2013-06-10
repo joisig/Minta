9bf64595c806762f978062484cd2e181bdb54964
Snorri Örn Daníelsson <snorri@orninn.com>
2013-06-10 19:17:49 +0000

    Added live results

--- extension.js
+++ extension.js
@@ -1,37 +1,49 @@
 $(document).ready(function(){
-	$('body *').each(function(){                                                                                                                               
-	    var obj = $(this).text();
-	    var match = obj.match(/^\$\d+(?:\.\d+)?\s*$/);
-	    if(match){
-	    	var number = Number(obj.replace(/[^0-9\.]+/g,""));
-	    	$(this).append('<span><div class="messagepop pop"><p>'+(number*120)+'isk</p></div><a id="peningar">ISK</a></span>');
-	    }                                                                                                                                   
-	});
+	var currencies = [];
+	//Fetches newest exchange rate
+	function getLatestExt(){
+		$.ajax({
+			type: "GET",
+			url: 'http://apis.is/currency/m5'
+		}).done(function(data){
+			currencies = data.results;
+			$('body *').each(function(){                                                                                                                               
+				var obj = $(this).text();
+				var match = obj.match(/^\$\d+(?:\.\d+)?\s*$/);
+				if(match){
+					var number = Number(obj.replace(/[^0-9\.]+/g,""));
+					$(this).append('<span><div class="messagepop pop"><p>'+Math.round(currencies[0].value*number)+' Kr</p></div><a id="peningar">ISK</a></span>');
+				}                                                                                                                                   
+			});
+		})
+	};
+	//Do this once
+	getLatestExt();
 
 
 	function deselect(element) {
-	    element.slideFadeToggle(function() { 
-	        element.next().removeClass("selected");
-	    });    
+		element.slideFadeToggle(function() { 
+			element.next().removeClass("selected");
+		});    
 	}
 
 	$(function() {
-	    $(document).on('click','#peningar', function() {
-	    	var tooltip = $(this).prev();
-	        if(tooltip.hasClass("selected")) {
-	            deselect(tooltip);               
-	        } else {
-	            tooltip.addClass("selected");
-	            tooltip.slideFadeToggle();
-	        }
-	        return false;
-	    });
+		$(document).on('click','#peningar', function() {
+			var tooltip = $(this).prev();
+			if(tooltip.hasClass("selected")) {
+				deselect(tooltip);               
+			} else {
+				tooltip.addClass("selected");
+				tooltip.slideFadeToggle();
+			}
+			return false;
+		});
 
-	   
+
 	});
 
 	$.fn.slideFadeToggle = function(easing, callback) {
-	    return this.animate({ opacity: 'toggle', height: 'toggle' }, "fast", easing, callback);
+		return this.animate({ opacity: 'toggle', height: 'toggle' }, "fast", easing, callback);
 	};
 
 });
