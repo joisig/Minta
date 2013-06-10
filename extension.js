fcabb93e20d1a69ce98a800fdbf9566921d48ce6
bjozzi <bjozzi@gmail.com>
2013-06-10 12:52:49 +0000

    Finnur dollara upphæðir og byrtir link þar við hlið sem opnar lítin popup glugga.

--- extension.js
+++ extension.js
@@ -1,6 +1,41 @@
 $(document).ready(function(){
-	var regex = new RegExp("\$?([0-9]{0,3},)*([0-9]{0,3})(\.[0-9]{2})?$')","gi");
+	/*var regex = new RegExp("\$?([0-9]{0,3},)*([0-9]{0,3})(\.[0-9]{2})?$')","gi");
 	var elementsFound = $("*:contains(regex)");
 	console.log(elementsFound.length);
 	console.log(elementsFound[0]);
+*/
+	$('body *').each(function(){                                                                                                                               
+	    var obj = $(this).text();
+	    var match = obj.match(/^\$\d+(?:\.\d+)?\s*$/);
+	    if(match){
+	    	var number = Number(obj.replace(/[^0-9\.]+/g,""));
+	    	$(this).append('<div class="messagepop pop"><p>'+(number*120)+'isk</p></div><a href="#" id="peningar">ISK</a>');
+	    }                                                                                                                                   
+	});
+
+
+	function deselect() {
+	    $(".pop").slideFadeToggle(function() { 
+	        $("#peningar").removeClass("selected");
+	    });    
+	}
+
+	$(function() {
+	    $("#peningar").on('click', function() {
+	        if($(this).hasClass("selected")) {
+	            deselect();               
+	        } else {
+	            $(this).addClass("selected");
+	            $(".pop").slideFadeToggle();
+	        }
+	        return false;
+	    });
+
+	   
+	});
+
+	$.fn.slideFadeToggle = function(easing, callback) {
+	    return this.animate({ opacity: 'toggle', height: 'toggle' }, "fast", easing, callback);
+	};
+
 });
