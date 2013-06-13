690db1ebd654cea01cb8407f43fd3f7069530298
Snorri Örn Daníelsson <snorri@orninn.com>
2013-06-13 17:26:51 +0000

    hmm

---
+++ background.js
@@ -0,0 +1,14 @@
+﻿function onLoad(){
+	chrome.contextMenus.create({
+		'title' : 'Breyta í ISK',
+		'contexts' : ['page'],
+		'onclick' : function(info, tab) {
+			chrome.tabs.getSelected(null, function(tab) {
+				console.log("Hello");
+				chrome.tabs.sendMessage(tab.id, {"getlastMouseDown": true}, function(response) {
+				});
+			});
+		}
+	});
+}
+onLoad();
